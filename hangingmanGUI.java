696e87c1b983ae297b71cec034e46ea0b17e5713
BeholdTheDevil <antdeg@outlook.com>
2016-10-24 14:12:50 +0200

    Inital commit with GUI-file and flow scheme

---
+++ hangingmanGUI.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.Style;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.BadLocationException;
+import java.io.*;
+
+public class hangingmanGUI extends JFrame{
+
+	private static JFrame mainframe;
+	private static JTextPane listWordBox;
+	private static 
+
+	public static void main(String[] args) {
+
+		getLine db = new getLine();
+		db.getLine();
+	}
+}
