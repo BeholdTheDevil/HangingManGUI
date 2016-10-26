8ea75b902e7d50609c4eca020e2a9aa19455ea59
BeholdTheDevil <antdeg@outlook.com>
2016-10-26 08:16:37 +0200

    Changes to the calling of functions. Now all done from GUI
    Removed callclass and added it's functionality to the GUI.

--- hangingmanGUI.java
+++ hangingmanGUI.java
@@ -12,8 +12,14 @@ import javax.swing.text.Style;
 import javax.swing.text.StyledDocument;
 import javax.swing.text.BadLocationException;
 import java.io.*;
+import java.util.Arrays;
 
-public class hangingmanGUI extends JFrame{
+public class hangingmanGUI extends JFrame {
+
+	/*
+	----------!            NUMBER OF FRAMES REQUIRED FOR HANGING = 11             !---------------
+	*/
+	public static int guesses = 11;
 
 	public static JFrame mainframe;
 	public static JTextPane wordList;
@@ -21,12 +27,22 @@ public class hangingmanGUI extends JFrame{
 	public static JTextField answerField;
 	public static JTextField userInput;
 	public static JButton submit;
+	public static JButton reset;
 	public static GridBagConstraints c;
 	public static Container panel;
+	public static char[][] savedWordArray;
 	public static char[] printedWord;
 	public static char[] answerWord;
 
+	public static void main(String[] args) {
+		//Get the randomized word from the dictionary
+		answerWord = "hej".toCharArray();//getLine.getWord().toCharArray();
+		//Initialize GUI
+		initGUI(answerWord.length);
+	}
+
 	public static GridBagConstraints gbc() {
+		//Zero out constraint preset so as to not accidently re-use the same constraints for multiple objects 
 		GridBagConstraints constraints = new GridBagConstraints();
 		return constraints;
 	}
@@ -35,6 +51,8 @@ public class hangingmanGUI extends JFrame{
 		initMainframe();
 		mainframe.setContentPane(panel);	
 		panel.setBackground(Color.darkGray);
+		//Create an array with the size [guesses][length of word];
+		savedWordArray = new char[guesses][length];
 
 		initHangMan();
 		initUserInput();
@@ -42,30 +60,47 @@ public class hangingmanGUI extends JFrame{
 		initSubmit();
 		initWordList();
 
+		//Create the placebo word
 		printedWord = new char[length*2];
-		for(int i = 1; i < length*2; i += 2) {
+		printedWord[0] = '_';
+		for(int i = 2; i < length*2; i += 2) {
 			printedWord[i-1] = ' ';
 			printedWord[i] = '_';
 		}
 		answerField.setText(String.valueOf(printedWord));
 
+		//Pack up and locate window at center of screen
 		mainframe.pack();	
    	    mainframe.setLocationRelativeTo(null);		
 		mainframe.setVisible(true);
 	}
 
 	public static void initMainframe() {
+		//Initialize mainframe with the title "Hanging Man"
 		mainframe = new JFrame("Hanging Man");  
+		//Set default exit operation
 		mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Set base mainframe size (will change dynamically with content)
 		mainframe.setSize(200, 200);
+		//Set layoutmanager to gridbaglayout
 		mainframe.setLayout(new GridBagLayout());
 
+		//Assign the main contentpane
 		panel = mainframe.getContentPane();
 	}
 
 	public static void initSubmit() {
+		//Create submit button
 		submit = new JButton("Submit");
+		//Add action event listener
+		submit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				submitActionPerformed(e);
+			}
+		});
 
+
+		//The following is the nulling of predefined constraints and the definition of new ones
 		c = gbc();
 		c.gridx = 3;
 		c.gridy = 2;
@@ -76,6 +111,73 @@ public class hangingmanGUI extends JFrame{
 		panel.add(submit, c);
 	}
 
+	public static void initReset() {
+		reset = new JButton("Reset");
+
+		reset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				resetActionPerformed(e);
+			}
+		});
+	}
+
+	public static void resetActionPerformed() {
+
+		guessCount = 0;
+		animation(0);
+		answerWord = getLine.getWord().toCharArray();
+		initGUI(answerWord.length);
+	}
+
+	public static void submitActionPerformed(ActionEvent e) throws NullPointerException {
+		//On actionevent trigger get current answer from answerfield
+		char[] currentAns = userInput.getText().toCharArray();
+		//Check for valid guess 
+		if(currentAns.length > 1 && compareToWordList(currentAns)) {
+
+			//Check if the char[] currentAns is equal to the char[] answerWord
+			if(Arrays.equals(currentAns, answerWord)) {
+				printedWord = answerWord;
+				//If it is you win the game
+				//	youWon();
+			} else {
+				//If it isn't, append the word to the wordlist box and animate hanging
+
+			//	animateHanging();
+			}
+
+			//If char[] is a single char
+		} else if(currentAns.length == 1) {
+			//Check if answer contains that character
+			for(int i = 0; i < answerWord.length; i++) {
+				if(answerWord[i] == currentAns[0]) {
+					//Check if that completes the word
+					printedWord[i*2] = currentAns[0];
+					if(String.valueOf(printedWord).replaceAll("\\s+","") == String.valueOf(answerWord)) {
+						//You have won!
+						//youWon();
+					}
+				}
+			}
+		}
+		//Add your guess to the list of guesses
+		wordList.setText(wordList.getText() + "\n" + String.valueOf(currentAns));
+		//Update the printed characters
+		answerField.setText(String.valueOf(printedWord));
+		//Remove characters from input box
+		userInput.setText("");
+	}
+
+	public static boolean compareToWordList(char[] currentAns) {
+
+		for(int i = 0; i < savedWordArray.length; i++) {
+			if(savedWordArray[i] == currentAns) {
+				return false;
+			}			
+		}
+		return true;
+	}
+
 	public static void initWordList() {
 		wordList = new JTextPane();
 		wordList.setText("Words guessed:");
@@ -83,14 +185,18 @@ public class hangingmanGUI extends JFrame{
 		wordList.setBackground(new Color(230, 230, 230));
 		wordList.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		wordList.setEditable(false);
+		//Create some internal padding in the textpane using borders and BorderFactory 
 		wordList.setBorder(
   			javax.swing.BorderFactory.createCompoundBorder(
       		javax.swing.BorderFactory.createTitledBorder(
       			null, " ", 
       			javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
       			javax.swing.border.TitledBorder.DEFAULT_POSITION,
-         		new java.awt.Font("Verdana", 2, 2)), javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1)));
+         		new java.awt.Font("Verdana", 2, 2)), javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1)
+      			)
+      		);
 
+		//The following is the nulling of predefined constraints and the definition of new ones
 		c = gbc();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridheight = 2;
@@ -99,7 +205,7 @@ public class hangingmanGUI extends JFrame{
 		c.gridx = 2;
 		c.gridy = 0;
 		c.weightx = 1.0;
-		c.insets = new Insets(10, 10, 10, 10);
+		c.insets = new Insets(10, 10, 10, 50);
 
 		panel.add(wordList, c);
 	}
@@ -111,11 +217,11 @@ public class hangingmanGUI extends JFrame{
 		animation.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		animation.setEditable(false);
 
+		//The following is the nulling of predefined constraints and the definition of new ones
 		c = gbc();
-		//c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 0;
-		c.insets = new Insets(10, 10, 0, 10);
+		c.insets = new Insets(50, 100, 10, 60);
 		c.anchor = GridBagConstraints.CENTER;
 
 		panel.add(animation, c);
@@ -123,17 +229,18 @@ public class hangingmanGUI extends JFrame{
 
 	public static void initAnswerField() {
 		answerField = new JTextField(2);
-		answerField.setPreferredSize(new Dimension(100, 25));
+		answerField.setPreferredSize(new Dimension(400, 25));
 		answerField.setBackground(new Color(180, 180, 180));
 		answerField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		answerField.setHorizontalAlignment(JTextField.CENTER);
 		answerField.setEditable(false);
 
+		//The following is the nulling of predefined constraints and the definition of new ones
 		c = gbc();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 1;
-		c.insets = new Insets(0, 10, 0, 10);
+		c.insets = new Insets(0, 50, 50, 10);
 		c.anchor = GridBagConstraints.CENTER;
 
 		panel.add(answerField, c);
@@ -146,6 +253,7 @@ public class hangingmanGUI extends JFrame{
 		userInput.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		userInput.setEditable(true);
 
+		//The following is the nulling of predefined constraints and the definition of new ones
 		c = gbc();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
